921d74edf2d350fd124a4a6eadfdd6aab447835b
João Gusmão <joao.gusmao@apiki.com>
2017-09-20 19:36:15 -0300

    fixing gulp to watch for all files and improving the base

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-const cssSource = './src/css/style.scss';
-
 gulp.task('sass', function () {
-	return gulp.src( cssSource )
+	return gulp.src( './src/css/style.scss' )
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch( cssSource , ['sass']);
+	gulp.watch( './src/css/**/*.scss' , ['sass']);
 });
